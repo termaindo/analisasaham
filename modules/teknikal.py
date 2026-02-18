ac77ae071c154b1fda00c5ebf2ec1f8aaa72ce38
termaindo <terma.indo@gmail.com>
2026-02-18 12:36:05 +0700

    Update teknikal.py

--- modules/teknikal.py
+++ modules/teknikal.py
@@ -6,7 +6,7 @@ from plotly.subplots import make_subplots
 import base64
 from datetime import datetime
 import os
-import pdfkit
+import fpdf
 
 # --- MENGGUNAKAN RELATIVE IMPORT SEBAGAI PENGAMAN ---
 try:
