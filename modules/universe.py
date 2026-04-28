8f4df2484277a8b7143a86c2a08e46e5a3f3ed39
termaindo <terma.indo@gmail.com>
2026-04-28 10:33:18 +0700

    Reorganize stock categories and add new sectors

--- modules/universe.py
+++ modules/universe.py
@@ -6,35 +6,42 @@ Cakupan: Kompas100, JII70, LQ45, IDX80, IDXG30, IDXQ30, IDXHIDIV20 + Mid-Cap Pil
 
 UNIVERSE_SAHAM = {
     "BANKING": [
-        "BBCA", "BBRI", "BMRI", "BBNI", "BBTN", "BRIS", "BTPS", "ARTO", "BBYB", "BDMN", 
-        "BNGA", "BJTM", "BJBR", "BBHI", "BBKP", "BVIC", "MAYA", "PNBN", "PNLF", "AGRO"
+        "AGRO", "ARTO", "BBCA", "BBRI", "BBYB", "BBHI", "BBKP", "BBNI", "BBTN", "BDMN", 
+        "BJBR", "BJTM", "BMRI", "BNGA", "BRIS", "BTPS", "BVIC", "MAYA", "NISP", "PNBN", 
+        "PNLF"
     ],
     "ENERGY_MINING": [
         "AADI", "ADRO", "ADMR", "ANTM", "BREN", "BUMI", "BRMS", "CUAN", "DSSA", "ELSA", 
         "ENRG", "HRUM", "INCO", "INDY", "ITMG", "MEDC", "PGAS", "PGEO", "PTBA", "RATU", 
-        "SGER", "TOBA", "TINS", "MBMA", "MDKA", "AMMN", "ESSA", "RAJA", "BIPI", "KKGI", "DOID"
+        "SGER", "TOBA", "TINS", "MBMA", "MDKA", "AMMN", "ESSA", "RAJA", "BIPI", "KKGI",
+        "DOID", "DEWA"
     ],
     "INFRASTRUCTURE": [
-        "TLKM", "ISAT", "EXCL", "MTEL", "JSMR", "TBIG", "TOWR", "WIFI", "INET", "WIKA", 
-        "ADHI", "PTPP", "ASSA", "POWR", "LINK", "IPTV", "META", "ACST", "ADCP", "BUKK"
+        "ACST", "ADCP", "ADHI", "ASSA", "BUKK", "EXCL", "INET", "IPTV", "ISAT", "JSMR",   
+        "LINK", "META", "MTEL", "POWR", "PTPP", "TBIG", "TOWR", "TLKM", "WIKA"
     ],
     "BASIC_INDUSTRIAL": [
         "ASII", "ARCI", "BRPT", "CPIN", "DSNG", "INKP", "INTP", "JPFA", "NCKL", "SMGR", 
         "TPIA", "TKIM", "UNTR", "AVIA", "AUTO", "SMSM", "DRMA", "AGII", "ARNA", "MAIN", 
-        "WOOD", "TOWR", "SMDR", "SRTG"
+        "WOOD", "TOWR", "SRTG"
     ],
     "CONSUMER_HEALTH": [
-        "AMRT", "CMRY", "GOTO", "HEAL", "ICBP", "INDF", "KLBF", "MIKA", "MYOR", "SIDO", 
+        "AMRT", "CMRY", "GGRM", "HEAL", "ICBP", "INDF", "KLBF", "MIKA", "MYOR", "SIDO", 
         "SILO", "UNVR", "ACES", "ERAA", "FILM", "MAPA", "MAPI", "HMSP", "MNCN", "BIRD", 
-        "MPMX", "ULTJ", "LPPF", "RALS", "SCMA", "TSPC", "KAEF", "BUKA", "BELI", "AMMS"
+        "MPMX", "ULTJ", "LPPF", "RALS", "SCMA", "TSPC", "KAEF", "AMMS", "HRTA", "WIIM"
     ],
     "PROPERTY": [
-        "BSDE", "CTRA", "PANI", "PWON", "SMRA", "BKSL", "ASRI", "KIJA", "MDLN", "APLN", 
-        "DILD", "SSIA", "LPCK"
+        "APLN", "ASRI", "BKSL", "BSDE", "CBDK", "CTRA", "DILD", "DMAS", "KIJA", "LPCK", 
+        "MDLN", "PANI", "PWON", "SMRA", "SSIA"
     ],
     "AGRI_MARITIME": [
         "LSIP", "AALI", "TAPG", "BWPT", "DSNG", "SIMP", "TBLA"
-    ]
+    ],
+    "TECHNOLOGY": [
+        "BELI", "BUKA", "GOTO", "WIFI"
+    ],
+    "TRANSPORTATION": [
+        "ELPI, "NELY", "SMDR", "TMAS"        
 }
 
 # 2. BENCHMARK VALUASI (Tetap sama, karena ini rata-rata sektor)
@@ -45,19 +52,26 @@ SECTOR_BENCHMARKS = {
     "BASIC_INDUSTRIAL": {"avg_per": 14.0, "avg_pbv": 1.6},
     "CONSUMER_HEALTH": {"avg_per": 22.0, "avg_pbv": 3.0},
     "PROPERTY": {"avg_per": 10.0, "avg_pbv": 0.8},
-    "AGRI_MARITIME": {"avg_per": 11.0, "avg_pbv": 1.0}
+    "AGRI_MARITIME": {"avg_per": 11.0, "avg_pbv": 1.0},
+    "TECHNOLOGY": {"avg_per": 15.0, "avg_pbv": 1.0},
+    "TRANSPORTATION": {"avg_per": 15.0, "avg_pbv": 1.0}
 }
 
 # 3. DATABASE SAHAM SYARIAH (ISSI) - Telah diperluas sesuai JII70 & ISSI terbaru
 SYARIAH_STOCKS_ISSI = {
-    "BRIS", "BTPS", "ADRO", "ADMR", "ANTM", "BREN", "BRMS", "ENRG", "HRUM", "INCO", 
-    "INDY", "ITMG", "PGAS", "PGEO", "PTBA", "TINS", "MDKA", "ESSA", "TLKM", "ISAT", 
-    "EXCL", "MTEL", "WIKA", "ADHI", "PTPP", "TOWR", "TBIG", "WIFI", "POWR", "ASII", 
-    "ARCI", "BRPT", "CPIN", "DSNG", "INKP", "INTP", "JPFA", "SMGR", "TPIA", "TKIM", 
-    "UNTR", "AUTO", "AVIA", "SMSM", "DRMA", "AMRT", "CMRY", "ICBP", "INDF", "KLBF", 
-    "MIKA", "MYOR", "SIDO", "SILO", "UNVR", "ACES", "ERAA", "MAPA", "MAPI", "FILM", 
-    "HEAL", "ULTJ", "BSDE", "CTRA", "PWON", "SMRA", "ASRI", "PANI", "LSIP", "AALI",
-    "TAPG", "AGII", "ARNA", "RAJA", "SGER", "TOBA", "MBMA", "CUAN", "BUKA", "BELI"
+    "AADI", "AALI", "ACES", "ACST", "ADCP", "ADHI", "ADMR", "ADRO", "AGII", "AMMN", 
+    "AMMS", "AMRT", "ANTM", "APLN", "ARCI", "ARNA", "ASII", "ASRI", "ASSA", "AUTO", 
+    "AVIA", "BELI", "BIPI", "BIRD", "BKSL", "BREN", "BRIS", "BRMS", "BRPT", "BSDE", 
+    "BTPS", "BUKA", "BUKK", "BUMI", "BWPT", "CBDK", "CMRY", "CPIN", "CTRA", "CUAN", 
+    "DEWA", "DILD", "DMAS", "DOID", "DRMA", "DSNG", "DSSA", "ELSA", "ENRG", "ERAA", 
+    "ESSA", "EXCL", "FILM", "GOTO", "HEAL", "HRTA", "HRUM", "ICBP", "INCO", "INDF", 
+    "INDY", "INET", "INKP", "INTP", "IPTV", "ISAT", "ITMG", "JPFA", "KAEF", "KIJA", 
+    "KKGI", "KLBF", "LINK", "LPCK", "LPPF", "LSIP", "MAIN", "MAPA", "MAPI", "MBMA", 
+    "MDKA", "MDLN", "MEDC", "META", "MIKA", "MNCN", "MPMX", "MTEL", "MYOR", "NCKL", 
+    "PANI", "PGAS", "PGEO", "POWR", "PTBA", "PTPP", "PWON", "RAJA", "RALS", "RATU", 
+    "SCMA", "SGER", "SIDO", "SILO", "SIMP", "SMDR", "SMGR", "SMRA", "SMSM", "SSIA", 
+    "TAPG", "TBLA", "TINS", "TKIM", "TLKM", "TOBA", "TPIA", "TSPC", "ULTJ", "UNTR", 
+    "UNVR", "WIFI", "WIKA", "WOOD", "ELPI", "NELY", "TMAS"
 }
 
 # --- Fungsi Helper Tetap Sama ---
